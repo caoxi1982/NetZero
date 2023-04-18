0fd6578deb093c2685ecabd7b9435d9432ac9db9
caoxi1982 <13861603553@163.com>
2023-04-18 08:18:50 +0800

    add V_bits Object for convert PLC INT to each bits

---
+++ ProjectFiles/NetSolution/ConfigOptix.cs
@@ -0,0 +1,21 @@
+#region Using directives
+using System;
+using UAManagedCore;
+using OpcUa = UAManagedCore.OpcUa;
+using FTOptix.HMIProject;
+using FTOptix.UI;
+using FTOptix.Retentivity;
+using FTOptix.NativeUI;
+using FTOptix.Core;
+using FTOptix.CoreBase;
+using FTOptix.NetLogic;
+#endregion
+
+public class ConfigOptix : BaseNetLogic
+{
+    [ExportMethod]
+    public void Method1()
+    {
+        // Insert code to be executed by the method
+    }
+}
